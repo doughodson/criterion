843cf006873fd8c675aefff7ff2297090660c5ad
Doug Hodson <doug@sidechannel.net>
2021-02-14 12:07:17 -0500

    updated code

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,17 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+
+#[inline]
+pub fn fibonacci(n: u64) -> u64 {
+    let mut a = 0;
+    let mut b = 1;
+    match n {
+        0 => b,
+        _ => {
+            for _ in 0..n {
+                let c = a + b;
+                a = b;
+                b = c;
+            }
+            b
+        }
     }
 }
