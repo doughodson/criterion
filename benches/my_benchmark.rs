c0e9b2dbde15c4c320fa33abd9862fa8afaa10da
Doug Hodson <doug@sidechannel.net>
2021-02-14 16:19:38 -0500

    update

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,9 +1,15 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use bench::fibonacci;
+use criterion::{BenchmarkId, criterion_group, criterion_main, Criterion};
+use benchmarks::{fibonacci_slow, fibonacci_fast};
 
-pub fn criterion_benchmark(c: &mut Criterion) {
-   c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+pub fn benchmark_fibs(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Fibonacci");
+    for i in [20u64, 21u64].iter() {
+       group.bench_with_input(BenchmarkId::new("Recursive", i), i, |b, i| b.iter(|| fibonacci_slow(*i)));
+       group.bench_with_input(BenchmarkId::new("Iterative", i), i, |b, i| b.iter(|| fibonacci_fast(*i)));
+    }
+    group.finish();
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, benchmark_fibs);
 criterion_main!(benches);
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
+#[inline]
+pub fn fibonacci_slow(n: u64) -> u64 {
+    match n {
+        0 => 1,
+        1 => 1,
+        n => fibonacci_slow(n-1) + fibonacci_slow(n-2),
+    }
+}
 
 #[inline]
-pub fn fibonacci(n: u64) -> u64 {
+pub fn fibonacci_fast(n: u64) -> u64 {
     let mut a = 0;
     let mut b = 1;
     match n {
@@ -15,3 +23,4 @@ pub fn fibonacci(n: u64) -> u64 {
         }
     }
 }
+
